4d0201bfadbb5b42c86a66871bef5ed92db745a2
luancsj <luancsj@gmail.com>
2021-09-27 09:30:57 -0300

    Add files via upload

---
+++ Ford =  random.py
@@ -0,0 +1,124 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[35]:
+
+
+def criptografia(palavra):
+    for i in ranged(len(palavra)):
+           if palavra == '21847542':
+                numero = palavra                    
+                numero = sorted(numero)
+                numero = ''.join(str(a) for a in numero)
+                numero = numero.replace("1", "F")
+                numero = numero.replace("8", "T")
+                numero = numero.replace("4", "F")
+                numero = numero.replace("2", "In")
+                numero = numero.replace("2", "Fi", 2)
+                print(numero)
+             else:
+                palavra=palavra.replace("", "a")
+                palavra=palavra.replace("F", "Q")
+                palavra= sorted(palavra)
+                palavra= ''.join(str(a) for a in palavra)
+                print(palavra)
+           
+              
+        
+        
+    
+
+
+# In[ ]:
+
+
+criptografia('Ford Motor Company')
+
+
+# In[ ]:
+
+
+criptografia('21847542')
+
+
+# In[21]:
+
+
+palavra = ('Ford Motor Company')
+
+
+# In[22]:
+
+
+palavra=palavra.replace("", "a")
+palavra=palavra.replace("F", "Q")
+palavra= sorted(palavra)
+palavra= ''.join(str(a) for a in palavra)
+print(palavra)
+
+
+# In[3]:
+
+
+print(out)
+
+
+# In[16]:
+
+
+palavra=palavra.replace("", "a")
+print(palavra)
+
+
+# In[25]:
+
+
+numero = ('21847542')
+numero = list(numero)
+numero = sorted(numero)
+numero = numero.replace("1", "F")
+numero = numero.replace("8", "T")
+numero = numero.replace("4", "F")
+numero = print(numero)
+
+
+# In[28]:
+
+
+numero = ('21847542')
+numero = list(numero)
+numero = sorted(numero)
+print(numero)
+
+
+# In[33]:
+
+
+numero = ('21847542')
+numero = sorted(numero)
+numero = ''.join(str(a) for a in numero)
+numero = numero.replace("1", "F")
+numero = numero.replace("8", "T")
+numero = numero.replace("4", "F")
+numero = numero.replace("2", "In")
+numero = numero.replace("2", "Fi", 2)
+print(numero)
+
+
+# In[13]:
+
+
+numero = ''.join(str(a) for a in numero)
+
+
+# In[14]:
+
+
+print(numero)
+
+
+# In[ ]:
+
+
+
+
